40b4c3685d673b2f83533a1c89b599c61088d19a
Julia Naomi <jnaomi@thoughtworks.com>
2017-08-31 00:46:06 -0300

    Hi Goku

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Ola Goku!!!")))
+    }).http("localhost:3000").unwrap();
 }
