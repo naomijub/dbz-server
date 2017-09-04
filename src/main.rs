fd85a42a7b587cb34a6911f7c182736c70602ffb
Julia Naomi <jnaomi@thoughtworks.com>
2017-09-04 17:55:24 -0300

    adding Iron struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 extern crate iron;
 
+use std::time::Duration;
+
 use iron::prelude::*;
 use iron::status;
+use iron::Timeouts;
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Ola Goku!!!")))
-    }).http("localhost:3000").unwrap();
+    Iron {
+        handler: |_: &mut Request| {Ok(Response::with((status::Ok, "Ola! Eu sou o Goku!!!")))},
+        threads: 8,
+        timeouts: Timeouts {
+            keep_alive: Some(Duration::from_secs(10)),
+            read: Some(Duration::from_secs(10)),
+            write: Some(Duration::from_secs(10))
+    }}.http("localhost:3000").unwrap();
 }
