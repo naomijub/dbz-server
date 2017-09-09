4d7144dbd105137b7518bd0f2eb492fdc5d8957f
Julia Naomi <jnaomi@thoughtworks.com>
2017-09-09 18:44:43 -0300

    router test

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,73 @@
 extern crate iron;
 extern crate iron_test;
-extern crate urlencoded;
+extern crate router;
 
 use iron::{Handler, status};
 use iron::prelude::*;
 
-use urlencoded::UrlEncodedBody;
+use router::Router;
 
-struct SpeechHandler;
+struct KiHandler;
+struct NameHandler;
 
-impl Handler for SpeechHandler {
+impl Handler for KiHandler {
     fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        let body = req.get_ref::<UrlEncodedBody>()
-                      .expect("UrlEnconding is not correct");
-        let name = body.get("name").unwrap()[0].to_owned();
-        let action = body.get("action").unwrap()[0].to_owned();
+        let params = req.extensions
+            .get::<router::Router>()
+            .expect("Router não encontrado a partir da extensão do Request.");
+        let ki = params.find("ki").unwrap();
 
-        Ok(Response::with((status::Ok, name + ", " + &action + "!")))
+        Ok(Response::with((status::Ok, "Eh mais de ".to_string() + ki)))
     }
 }
 
+impl Handler for NameHandler {
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        let params = req.extensions
+            .get::<router::Router>()
+            .expect("Router não encontrado a partir da extensão do Request.");
+        let name = params.find("name").unwrap().to_owned();
+
+        Ok(Response::with((status::Ok, name + "\nEu sou Goku!")))
+    }
+}
+
+fn app_router() -> Router {
+    let mut router = Router::new();
+    router.get("valor/:ki", KiHandler, "ki router")
+          .get("nome/:name", NameHandler, "name router");
+    router
+}
+
 fn main() {
-    Iron::new(SpeechHandler).http("localhost:3000").unwrap();
+    Iron::new(app_router()).http("localhost:3000").unwrap();
 }
 
 #[cfg(test)]
 mod test {
     use iron::Headers;
-    use iron::headers::ContentType;
 
     use iron_test::{request, response};
 
-    use super::SpeechHandler;
+    use super::{app_router};
 
     #[test]
-    fn test_body() {
-        let mut headers = Headers::new();
-                headers.set(ContentType("application/x-www-form-urlencoded".parse().unwrap()));
-        let response = request::post("http://localhost:3000/",
-                                     headers,
-                                     "name=Goku&action=Morra",
-                                     &SpeechHandler);
-        let result = response::extract_body_to_string(response.unwrap());
+    fn test_router_ki() {
+        let response = request::get("http://localhost:3000/valor/8000",
+                                    Headers::new(),
+                                    &app_router());
+        let result = response::extract_body_to_bytes(response.unwrap());
 
-        assert_eq!(result, "Goku, Morra!");
+        assert_eq!(result, b"Eh mais de 8000");
+    }
+
+    #[test]
+    fn test_router_name() {
+        let response = request::get("http://localhost:3000/nome/Vegita",
+                                    Headers::new(),
+                                    &app_router());
+        let result = response::extract_body_to_bytes(response.unwrap());
+
+        assert_eq!(result, b"Vegita\nEu sou Goku!");
     }
 }
